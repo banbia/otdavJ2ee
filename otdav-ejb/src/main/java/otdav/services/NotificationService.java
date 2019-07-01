af699484aa1fd2dcf71bb5170ad5d513c3780a0c
unknown <soumayazammali@gmail.com>
2019-07-01 19:11:52 +0100

    services package/notif

---
+++ otdav-ejb/src/main/java/otdav/services/NotificationService.java
@@ -0,0 +1,18 @@
+package otdav.services;
+
+
+import javax.ejb.Remote;
+import otdav.entities.Notification;;
+
+@Remote
+public interface NotificationService {
+	
+	
+ public void addrNotification(Notification notification);
+ public void updateNotification(Notification notification);
+ public void deleteNotification(Notification notification);
+ public void selectNotification(Notification notification);
+ public void searchNotification(Notification notification);
+ 
+ 
+}
